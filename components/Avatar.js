7045e554fae2966fb8adcc2a7aa031795b7b29c1
Nathan Pare <nathanpare@Nathans-MBP.lan>
2023-01-23 23:28:48 -0800

    complete search header

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Avatar = ({ url }) => {
+const Avatar = ({ url, className }) => {
   return (
     <img
-      className='h-8 rounded-full cursor-pointer transition duration-150
-       transform hover:scale-110'
+      className={`h-8 rounded-full cursor-pointer transition duration-150
+       transform hover:scale-110 ${className}`}
       loading='lazy'
       src={url}
       alt="profile pic"
--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,55 @@
 import React, { useRef } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { MicrophoneIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon, MicrophoneIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import Avatar from './Avatar';
+import HeaderOptions from './HeaderOptions';
 
 const Header = () => {
   const router = useRouter();
   const searchInputRef = useRef(null);
 
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  }
+
   return (
-    <header>
-      <Image
-        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-        height={40}
-        width={120}
-        className="cursor-pointer"
-        onClick={() => router.push("/")}
-      />
-      <form className='flex px-6 py-3 ml-10 mr-5 border border-gray-200
+    <header className='sticky top-0 bg-white'>
+      <div className='flex w-full p-6 items-center'>
+        <Image
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={40}
+          width={120}
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
+        />
+        <form className='flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200
       rounded-full shadow-lg max-w-3xl items-center'>
-        <input
-          ref={searchInputRef}
-          className="flex-grow w-full focus:outline-none"
-          type="text"
-        />
-        <XMarkIcon className='h-7 sm:mr-3 text-gray-500 cursor-pointer transition
+          <input
+            ref={searchInputRef}
+            className="flex-grow w-full focus:outline-none"
+            type="text"
+          />
+          <XMarkIcon className='h-7 sm:mr-3 text-gray-500 cursor-pointer transition
          duration-100 transform hover:scale-125'
-          onClick={searchInputRef.current.value = ""}
+            onClick={() => (searchInputRef.current.value = "")}
+          />
+          <MicrophoneIcon hidden className='mr-3 h-6 sm:inline-flex text-blue-500
+         border-l-2 pl-4 border-gray-300' />
+          <MagnifyingGlassIcon className='h-6 text-blue-500 hidden sm:inline-flex' />
+          <button hidden type='submit' onClick={search}>Search</button>
+        </form>
+        <Avatar
+         className="ml-auto"
+         url="https://upload.wikimedia.org/wikipedia/en/6/6f/KennyMcCormick.png"
         />
-        <MicrophoneIcon className='mr-3 h-6 sm:inline-flex text-blue-500
-         border-l-2 ' />
-      </form>
+      </div>
+      <HeaderOptions />
     </header>
   )
 }
